4ef6fdc7cc1a6be336b758bde79ba53c42ced136
yuelixing <1223343444@qq.com>
2016-11-30 12:25:47 +0800

    update

--- setup.sh
+++ setup.sh
@@ -1,4 +1,6 @@
 #! /bin/bash
 SRC_HOME=`pwd`
+echo "import start"
 cp -af ${SRC_HOME}/CodeSnippets/*.codesnippet ~/Library/Developer/Xcode/UserData/CodeSnippets/
-echo "done"
+echo "import finish"
+echo "重启 Xcode 生效"
