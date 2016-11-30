8a798b45d82a02dc5e90a4004c11bb22aba5e696
yuelixing <1223343444@qq.com>
2016-11-30 12:14:04 +0800

    update

--- commit.sh
+++ commit.sh
@@ -3,5 +3,6 @@ SRC_HOME=`pwd`
 rm -rdf CodeSnippets/*.codesnippet
 cp -af ~/Library/Developer/Xcode/UserData/CodeSnippets/*.codesnippet ${SRC_HOME}/CodeSnippets/
 
+git add *
 git commit -a -m "update"
 git push -u origin master
