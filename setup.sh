87e8d081976a250e5b0bfb4e7bb86a31b3836539
yuelixing <yuelixing@yuelixingdeiMac.local>
2020-11-25 13:06:11 +0800

    添加检查

--- setup.sh
+++ setup.sh
@@ -1,6 +1,7 @@
 #! /bin/bash
 SRC_HOME=`pwd`
 echo "import start"
+mkdir -p ~/Library/Developer/Xcode/UserData/CodeSnippets/
 cp -af ${SRC_HOME}/CodeSnippets/*.codesnippet ~/Library/Developer/Xcode/UserData/CodeSnippets/
 echo "import finish"
 echo "重启 Xcode 生效"
